3ae613541daa31a00fcf65ade31fbe4703f36076
Dmitry <isyzes@gmail.com>
2020-02-20 00:26:40 +0300

    added H2 db test

--- src/main/java/com/example/demo/security/Beans.java
+++ src/main/java/com/example/demo/security/Beans.java
@@ -1,9 +1,14 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class Beans {
--- src/test/java/com/example/demo/controller/IngredientControllerTest.java
+++ src/test/java/com/example/demo/controller/IngredientControllerTest.java
@@ -1,40 +1,81 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserSignInResponse;
 import com.example.demo.mockdata.ControllerMockData;
 import com.example.demo.repository.IngredientRepository;
+import com.example.demo.security.LoadUserDetailService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static com.example.demo.security.Roles.MANAGER;
+import static org.hamcrest.Matchers.hasLength;
+import static org.mockito.BDDMockito.willReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @TestPropertySource("classpath:application-test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class IngredientControllerTest extends AbstractControllerTest {
-
+public class IngredientControllerTest {
 
     @Autowired
-    private IngredientRepository ingredient;
+    protected MockMvc mockMvc;
+    @Autowired
+    protected ObjectMapper objectMapper;
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
+    @Autowired
+    protected IngredientRepository ingredientRepository;
+    @SpyBean
+    protected LoadUserDetailService loadUserDetailService;
 
     @Test
     public void testBuyIngredientIsOk() throws Exception {
 
-        ingredient.save(ControllerMockData.getNewIngredient());
+        ingredientRepository.save(ControllerMockData.getNewIngredient());
 
-        final String token = signIn(MANAGER);
-
-//        given(ingredientRepository.findById(ControllerMockData.ID)).willReturn(ControllerMockData.getNewOptionalIngredient());
+        final String token = signIn();
 
         mockMvc.perform(put("/ingredients/buy").header("Authorization", token)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"idIngredient\" : \"3\", \"milligramsInStock\" : 2551}"))
+                .content("{\"idIngredient\" : \"1\", \"milligramsInStock\" : 2551}"))
 
                 .andExpect(status().isOk());
     }
+
+    private String signIn() throws Exception {
+        final User user = new User("vasya@email.com", passwordEncoder.encode("qwerty"),
+                List.of(new SimpleGrantedAuthority("ROLE_" + MANAGER.name())));
+
+        willReturn(user).given(loadUserDetailService).loadUserByUsername("vasya@email.com");
+
+        final String response = mockMvc.perform(post("/employee/sign-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "  \"email\" : \"vasya@email.com\",\n" +
+                        "  \"password\" : \"qwerty\"\n" +
+                        "}"))
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("token", hasLength(144)))
+                .andReturn().getResponse().getContentAsString();
+
+        return "Bearer " + objectMapper.readValue(response, UserSignInResponse.class).getToken();
+    }
+
+
 }
